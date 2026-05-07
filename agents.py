e247036475e34a8cbb92565fa4f692f037b6771c
Nova <novamaster600@gmail.com>
2026-05-07 16:50:38 +0530

    Refactor imports and reorganize agent definitions

--- agents.py
+++ agents.py
@@ -3,7 +3,7 @@ import os
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from Langchain_Projects.Multi_Research_Agent.tools import scrap_text, search_tool
+from tools import scrap_text, search_tool
 
 os.environ["NVIDIA_API_KEY"] = "nvapi-xxxx" #add your api key
 chat_llm = ChatNVIDIA(model="openai/gpt-oss-120b", temperature=0.7,max_completion_tokens=4000, )
